08d058e8dd561734284d032cf5c43337ddda687b
vasiliy <vasylevochcin@mail.ru>
2026-05-14 13:21:44 +0300

    Levochlin new function

--- dice.asm
+++ dice.asm
@@ -1,9 +1,11 @@
 format ELF64
+
 public _start
 
 section '.bss' writable
     urandom_fd  dq 0
-    balance     dq 0
+    balance     dq 0          ; счет игрока
+    casino_bal  dq 0          ; счет казино
     guess       dq 0
     bet         dq 0
     dice_sum    dq 0
@@ -15,7 +17,8 @@ section '.bss' writable
 section '.data' writable
     urandom_path db '/dev/urandom', 0
 
-    msg_bal      db 0x0A, '====================', 0x0A, 'Текущие очки: ', 0
+    msg_bal      db 0x0A, '====================', 0x0A, 'Игрок: ', 0
+    msg_casino   db ' | Казино: ', 0
     msg_guess    db 'Введите задуманное число (2-12, или 0 для выхода): ', 0
     msg_bet      db 'Введите вашу ставку: ', 0
     msg_roll     db 'Выпали кубики: ', 0
@@ -25,85 +28,96 @@ section '.data' writable
     msg_win4     db 0x0A, '>>> Точное совпадение! Вы выиграли ставку в 4-кратном размере!', 0x0A, 0
     msg_win1     db 0x0A, '>>> Совпадение по диапазону! Вы выиграли ставку!', 0x0A, 0
     msg_lose     db 0x0A, '>>> Неудача. Ставка проиграна.', 0x0A, 0
-    msg_over     db 0x0A, 'Игра окончена. У вас закончились очки.', 0x0A, 0
-
+    msg_game_over db 0x0A, 'Игра окончена. У одного из участников закончились очки.', 0x0A, 0
+    msg_casino_bankrupt db 0x0A, 'Казино обанкротилось! Вы победили!', 0x0A, 0
+    
     newline      db 0x0A, 0
 
 section '.text' executable
 
 _start:
-    ; Инициализация начальных очков (100)
-    mov qword [balance], 100
+    ; Начальные очки
+    mov qword [balance], 100     ; у игрока 100 очков
+    mov qword [casino_bal], 1000 ; у казино 1000 очков
 
-    ; Открываем /dev/urandom для получения случайных чисел
-    mov rax, 2          ; syscall: sys_open
+    ; Открываем /dev/urandom
+    mov rax, 2
     mov rdi, urandom_path
-    mov rsi, 0          ; флаг O_RDONLY
+    mov rsi, 0
     syscall
     test rax, rax
-    js exit_program     ; если ошибка открытия - выходим
+    js exit_program
     mov [urandom_fd], rax
 
 game_loop:
-    ; Проверка на проигрыш (баланс <= 0)
+    ; Проверка на окончание игры
     cmp qword [balance], 0
     jle game_over
+    cmp qword [casino_bal], 0
+    jle casino_bankrupt
 
-    ; Вывод текущего баланса
+    ; Вывод текущих балансов
     mov rdi, msg_bal
     call print_string
     mov rax, [balance]
     call print_num
+    mov rdi, msg_casino
+    call print_string
+    mov rax, [casino_bal]
+    call print_num
     mov rdi, newline
     call print_string
 
-    ; Вывод запроса на задуманное число
+    ; Запрос задуманного числа
     mov rdi, msg_guess
     call print_string
-
-    ; Чтение числа
     call read_num
     mov [guess], rax
 
-    ; Если ввели 0 - выход из игры
+    ; Выход если ввели 0
     cmp qword [guess], 0
     je close_input
 
-    ; Вывод запроса на ставку
+    ; Запрос ставки
     mov rdi, msg_bet
     call print_string
-
-    ; Чтение ставки
     call read_num
     mov [bet], rax
+    
+    ; Проверка: ставка не больше счета игрока
+    mov rax, [bet]
+    cmp rax, [balance]
+    jg game_loop        ; недостаточно у игрока
+    
+    ; Проверка: у казино достаточно денег на максимальный выигрыш (ставка * 4)
+    mov rbx, rax
+    shl rbx, 2          ; rbx = bet * 4
+    cmp rbx, [casino_bal]
+    jg game_loop        ; казино не может выплатить максимальный выигрыш
 
-    ; ---- БРОСОК КУБИКОВ ----
-    ; Читаем 2 случайных байта из /dev/urandom
-    ; ---- БРОСОК КУБИКОВ ----
-    mov rax, 0          ; syscall: sys_read
+    ; Бросок кубиков
+    mov rax, 0
     mov rdi, [urandom_fd]
     mov rsi, buffer
     mov rdx, 2
     syscall
 
-    ; Вычисляем 1-й кубик: (byte1 % 6) + 1
+    ; Первый кубик
     movzx ax, byte [buffer]
     mov cl, 6
-    div cl              ; al = результат, ah = остаток (0-5)
+    div cl
+    mov al, ah
+    inc al
+    movzx r12, al
 
-    mov al, ah          ; Переносим остаток в AL, чтобы избежать конфликта MOVZX
-    inc al              ; сдвигаем в диапазон 1-6
-    movzx r12, al       ; Теперь r12 безопасно получает значение
-
-    ; Вычисляем 2-й кубик: (byte2 % 6) + 1
+    ; Второй кубик
     movzx ax, byte [buffer+1]
     div cl
-
-    mov al, ah          ; Переносим остаток в AL
+    mov al, ah
     inc al
-    movzx r13, al       ; r13 = второй кубик
-    
-    ; Считаем сумму
+    movzx r13, al
+
+    ; Сумма кубиков
     mov r14, r12
     add r14, r13
     mov [dice_sum], r14
@@ -124,60 +138,68 @@ game_loop:
     mov rdi, newline
     call print_string
 
-    ; ---- ИГРОВАЯ ЛОГИКА И ПРОВЕРКА УСЛОВИЙ ----
+    ; Игровая логика
     mov rax, [dice_sum]
     mov rbx, [guess]
     mov r10, [bet]
 
-    ; 1. Проверка на точное совпадение
+    ; Точное совпадение?
     cmp rax, rbx
     je exact_win
 
-    ; 2. Проверка диапазонов (меньше 7 или больше 7)
+    ; Проверка диапазонов
     cmp rax, 7
     jl check_low
     jg check_high
-    jmp lose_bet        ; Если сумма ровно 7, но мы не угадали точно - проигрыш
+    jmp lose_bet
 
 check_low:
     cmp rbx, 7
-    jl range_win        ; Сумма < 7 и задумано < 7
+    jl range_win
     jmp lose_bet
 
 check_high:
     cmp rbx, 7
-    jg range_win        ; Сумма > 7 и задумано > 7
+    jg range_win
     jmp lose_bet
 
 exact_win:
-    ; Выигрыш = ставка * 4
+    ; Выигрыш: ставка * 4
     mov rax, r10
-    shl rax, 2          ; Быстрое умножение на 4 через сдвиг
-    add [balance], rax
+    shl rax, 2
+    add [balance], rax   ; игрок получает
+    sub [casino_bal], rax ; казино теряет
     mov rdi, msg_win4
     call print_string
     jmp game_loop
 
 range_win:
-    ; Выигрыш = ставка
+    ; Выигрыш: ставка
     add [balance], r10
+    sub [casino_bal], r10
     mov rdi, msg_win1
     call print_string
     jmp game_loop
 
 lose_bet:
-    ; Проигрыш = минус ставка
+    ; Проигрыш: теряем ставку
     sub [balance], r10
+    add [casino_bal], r10
     mov rdi, msg_lose
     call print_string
     jmp game_loop
 
 game_over:
-    mov rdi, msg_over
+    mov rdi, msg_game_over
+    call print_string
+    jmp close_input
+
+casino_bankrupt:
+    mov rdi, msg_casino_bankrupt
     call print_string
 
 close_input:
-    ; Закрываем файловый дескриптор urandom (аналог твоего close_input)
+    ; Закрываем /dev/urandom
     mov rax, 3
     mov rdi, [urandom_fd]
     syscall
@@ -188,11 +210,9 @@ exit_program:
     syscall
 
 ; =========================================
-; ПОДПРОГРАММЫ (Helpers)
+; ПОДПРОГРАММЫ
 ; =========================================
 
-; Печать строки (использует твой алгоритм strlen_loop)
-; Вход: RDI - указатель на нуль-терминированную строку
 print_string:
     push rax
     push rdi
@@ -207,13 +227,11 @@ print_string:
     inc rax
     jmp .strlen_loop
 .strlen_done:
-    mov rdx, rax        ; длина строки
-
+    mov rdx, rax
     test rdx, rdx
     jz .print_done
-
-    mov rax, 1          ; sys_write
-    mov rdi, 1          ; stdout
+    mov rax, 1
+    mov rdi, 1
     syscall
 .print_done:
     pop rdx
@@ -222,8 +240,6 @@ print_string:
     pop rax
     ret
 
-; Печать целого числа (itoa)
-; Вход: RAX - число для вывода
 print_num:
     push rax
     push rbx
@@ -239,8 +255,8 @@ print_num:
 
 .itoa_loop:
     xor rdx, rdx
-    div rbx             ; делим на 10
-    add dl, '0'         ; переводим остаток в символ ASCII
+    div rbx
+    add dl, '0'
     mov [rsi], dl
     dec rsi
     test rax, rax
@@ -258,8 +274,6 @@ print_num:
     pop rax
     ret
 
-; Чтение ввода пользователя (atoi)
-; Выход: RAX - введенное число
 read_num:
     push rbx
     push rcx
@@ -267,8 +281,8 @@ read_num:
     push rdi
     push rsi
 
-    mov rax, 0          ; sys_read
-    mov rdi, 0          ; stdin
+    mov rax, 0
+    mov rdi, 0
     mov rsi, buffer
     mov rdx, BUFFER_SIZE
     syscall
@@ -279,14 +293,14 @@ read_num:
 
 .atoi_loop:
     movzx rcx, byte [rsi]
-    cmp rcx, 0x0A       ; конец строки (Enter)
+    cmp rcx, 0x0A
     je .atoi_done
     cmp rcx, '0'
     jl .atoi_done
     cmp rcx, '9'
     jg .atoi_done
 
-    sub rcx, '0'        ; ASCII в число
+    sub rcx, '0'
     imul rax, 10
     add rax, rcx
     inc rsi
